4b52af9ab6df1ee12543c491749a601c64748fa7
Adebowale <adebowaletemitayo40@gmail.com>
2023-06-24 15:30:37 +0100

    random dog breeds

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import List from "./dogList";
+import Alert from "./Alert";
+import "./App.css";
+import { useGlobalContext } from "./context";
 
-function App() {
+const data = "me";
+const url = "https://dog.ceo/api/breed/${data}/images/random";
+// `https://dog.ceo/api/breeds/${dogName}/images/random`
+
+const App = () => {
+  const {
+    dogList,
+    setDogList,
+    dogName,
+    setDogName,
+    editID,
+    setEditID,
+    showAlert,
+    removeItem,
+    editItem,
+    clearList,
+    setIsEditing,
+    isEditing,
+    alert,
+    data,
+    setData,
+  } = useGlobalContext();
+
+  const fetchData = async () => {
+    try {
+      // setDogName(e.target.value);
+      const response = await fetch(
+        `https://dog.ceo/api/breed/${dogName.toLowerCase()}/images/random`
+      );
+      const data = await response.json();
+      setData(data);
+      console.log(String(data.message));
+    } catch (error) {
+      return setData(data.message);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!dogName) {
+      showAlert(true, "danger", "please enter value");
+    } else if (dogName && isEditing) {
+      setDogList(
+        dogList.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: dogName };
+          }
+          return item;
+        })
+      );
+      setDogName("");
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true, "success", "value changed");
+    } else {
+      fetchData();
+      showAlert(true, "success", "item added to the list");
+
+      const newItem = { id: new Date().getTime().toString(), title: dogName };
+      setDogList([...dogList, newItem]);
+      setDogName("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="section-center">
+        <form className="grocery-form" onSubmit={handleSubmit}>
+          {alert.show && (
+            <>
+              <Alert {...alert} removeAlert={showAlert} list={dogList} />
+            </>
+          )}
+          {data.status === "success" ? (
+            <div className="image-container">
+              <img src={`${data.message}`} alt="dog" className="image" />
+            </div>
+          ) : (
+            <h3 style={{ color: "red" }}>{data.message}</h3>
+          )}
+
+          <h3>Random Dog Breeds</h3>
+
+          <div className="form-control">
+            <input
+              type="text"
+              className="grocery"
+              placeholder="e.g. Rottweiler"
+              value={dogName}
+              onChange={(e) => setDogName(e.target.value)}
+            />
+            <button type="submit" className="submit-btn">
+              {isEditing ? "edit" : "submit"}
+            </button>
+          </div>
+        </form>
+
+        {dogList.length > 0 && (
+          <div className="grocery-container">
+            <List items={dogList} removeItem={removeItem} editItem={editItem} />
+            <button className="clear-btn" onClick={clearList}>
+              clear items
+            </button>
+          </div>
+        )}
+      </section>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/context.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext, useEffect } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [dogName, setDogName] = useState("");
+  const [data, setData] = useState([]);
+  const [dogList, setDogList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setDogList([]);
+    setData([]);
+  };
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setDogList(dogList.filter((item) => item.id !== id));
+  };
+  const editItem = (id) => {
+    const specificItem = dogList.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setDogName(specificItem.title);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        dogList,
+        setDogList,
+        editID,
+        setEditID,
+        showAlert,
+        clearList,
+        removeItem,
+        editItem,
+        setDogName,
+        dogName,
+        isEditing,
+        setIsEditing,
+        alert,
+        data,
+        setData,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
